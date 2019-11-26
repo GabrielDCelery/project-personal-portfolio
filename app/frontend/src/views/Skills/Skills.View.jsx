2466d66d38a3fd1777672acf8e8c15d55c907c23
Gabriel Zeller <gabriel.d.celery@gmail.com>
2019-11-26 22:21:49 +0000

    - Working on skills section

--- app/frontend/src/views/Skills/Skills.View.jsx
+++ app/frontend/src/views/Skills/Skills.View.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Box, Grid } from '@material-ui/core';
+import { ComponentLeftAlignedContainer } from 'components';
 
 export default function SkillsView({ getter, handler }) {
-  return <React.Fragment>Skills</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Box height="2em" />
+      <ComponentLeftAlignedContainer maxWidth="xl">
+        a
+      </ComponentLeftAlignedContainer>
+    </React.Fragment>
+  );
 }
