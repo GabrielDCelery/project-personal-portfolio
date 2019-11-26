ff41e57d8e56ee62c75176e4f30cb784c0a8496a
Gabriel Zeller <gabriel.d.celery@gmail.com>
2019-11-26 21:28:52 +0000

    - Reworking Achievments section

--- app/frontend/src/views/Achievments/Achievments.View.jsx
+++ app/frontend/src/views/Achievments/Achievments.View.jsx
@@ -1,34 +1,7 @@
 import React from 'react';
-import {
-  //Container,
-  Button,
-  ExpansionPanel,
-  ExpansionPanelDetails,
-  Box,
-  Grid,
-  Link,
-  Typography,
-  ExpansionPanelSummary
-} from '@material-ui/core';
-import {
-  ExpandMore as ExpandMoreIcon,
-  GitHub as GitHubIcon
-} from '@material-ui/icons';
-import {
-  ComponentExpansionPanelHeading,
-  ComponentLeftAlignedContainer,
-  ComponentParagraphHeading,
-  AchievmentCard
-} from 'components';
-import unleashTheGeekPreviewImage from 'assets/achievments/unleash_the_geek_001.jpg';
-import halitePreviewImage from 'assets/achievments/halite3_001.jpg';
-import config from 'config';
-//import { SizeMe } from 'react-sizeme';
+import { Box, Grid } from '@material-ui/core';
 
-const previewImageSrcMap = {
-  unleashTheGeekPreviewImage,
-  halitePreviewImage
-};
+import { ComponentLeftAlignedContainer, AchievmentCard } from 'components';
 
 export default function AchievmentsView({ getter, handler }) {
   return (
@@ -52,12 +25,16 @@ export default function AchievmentsView({ getter, handler }) {
             ) => {
               return (
                 <React.Fragment key={`competition-item-${index}`}>
-                  <Grid item sm={12} md={4}>
+                  <Grid item md={12} lg={4}>
                     <AchievmentCard
                       title={name}
                       subTitle={type}
                       description={description}
                       previewImage={previewImage}
+                      finalPlace={finalPlace}
+                      numOfCompetitors={numOfCompetitors}
+                      codeUrl={codeUrl}
+                      reportUrl={reportUrl}
                     />
                   </Grid>
                 </React.Fragment>
@@ -67,131 +44,6 @@ export default function AchievmentsView({ getter, handler }) {
         </Grid>
       </ComponentLeftAlignedContainer>
 
-      <Box height="10em" />
-
-      <ComponentLeftAlignedContainer maxWidth="xl">
-        {getter('items').map(
-          (
-            {
-              name,
-              type,
-              codeUrl,
-              reportUrl,
-              description,
-              finalPlace,
-              numOfCompetitors,
-              previewImage
-            },
-            index
-          ) => {
-            return (
-              <React.Fragment key={`competition-item-${index}`}>
-                <ExpansionPanel
-                  expanded={getter('isPanelOpen')(index)}
-                  onChange={() => {
-                    handler('setExpandedPanel')(index);
-                  }}
-                >
-                  <ExpansionPanelSummary
-                    expandIcon={
-                      <ExpandMoreIcon
-                        style={{ color: config.styles.colors.darkBgText }}
-                      />
-                    }
-                    style={{
-                      backgroundColor: config.styles.colors.primary,
-                      padding: '0 2em'
-                    }}
-                  >
-                    <ComponentExpansionPanelHeading
-                      labelFirst={name}
-                      labelSecond={type}
-                      labelColor={config.styles.colors.darkBgText}
-                      bgColor={config.styles.colors.primary}
-                      fontSize="1.3rem"
-                    />
-                  </ExpansionPanelSummary>
-
-                  <ExpansionPanelDetails style={{ padding: 0 }}>
-                    <Box width="100%">
-                      <ComponentParagraphHeading
-                        label="Results"
-                        bgColor={config.styles.colors.secondary}
-                        labelColor={config.styles.colors.darkBgText}
-                      />
-
-                      <Box p="0">
-                        <Grid container>
-                          <Grid item md={6} sm={12}>
-                            <Box p="1em" textAlign="center">
-                              <Link
-                                href={reportUrl}
-                                target="_blank"
-                                underline="none"
-                              >
-                                <Button
-                                  fullWidth
-                                  variant="outlined"
-                                  color="primary"
-                                  size="large"
-                                >
-                                  {`Final Place ${finalPlace} out of ${numOfCompetitors}`}
-                                </Button>
-                              </Link>
-                            </Box>
-                          </Grid>
-
-                          <Grid item md={6} sm={12}>
-                            <Box p="1em" textAlign="center">
-                              <Link
-                                href={codeUrl}
-                                target="_blank"
-                                underline="none"
-                              >
-                                <Button
-                                  fullWidth
-                                  variant="outlined"
-                                  color="primary"
-                                  size="large"
-                                >
-                                  Link to project (with post mortem){' '}
-                                  <GitHubIcon style={{ marginLeft: '10px' }} />
-                                </Button>
-                              </Link>
-                            </Box>
-                          </Grid>
-                        </Grid>
-                      </Box>
-
-                      <ComponentParagraphHeading
-                        label="Description"
-                        bgColor={config.styles.colors.secondary}
-                        labelColor={config.styles.colors.darkBgText}
-                      />
-                      <Box p="2em">
-                        <Typography paragraph={true}>{description}</Typography>
-                      </Box>
-
-                      <ComponentParagraphHeading
-                        label="Screenshot"
-                        bgColor={config.styles.colors.secondary}
-                        labelColor={config.styles.colors.darkBgText}
-                      />
-                      <Box p="0">
-                        <img
-                          src={previewImageSrcMap[previewImage]}
-                          alt="Unleash the Geek preview"
-                          style={{ width: '100%' }}
-                        />
-                      </Box>
-                    </Box>
-                  </ExpansionPanelDetails>
-                </ExpansionPanel>
-              </React.Fragment>
-            );
-          }
-        )}
-      </ComponentLeftAlignedContainer>
       <Box height="4em" />
     </React.Fragment>
   );
