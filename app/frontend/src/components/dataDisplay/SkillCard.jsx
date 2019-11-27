5e94cce053842080e891c46563caf24c69f91f98
Gabriel Zeller <gabriel.d.celery@gmail.com>
2019-11-27 22:34:59 +0000

    - Working on Skills section

--- app/frontend/src/components/dataDisplay/SkillCard.jsx
+++ app/frontend/src/components/dataDisplay/SkillCard.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
 
-import { Box, Card, Divider, Grid, Icon } from '@material-ui/core';
-import mysql from 'assets/mysql.svg';
+import { Box, Card, Divider, Grid, Icon, Typography } from '@material-ui/core';
+import { SizeMe } from 'react-sizeme';
 
 export const SkillCard = props => {
+  const { iconSrc, label, cardBgColor, iconBorderColor } = props;
+
   return (
-    <Card>
+    <Card
+      style={{
+        backgroundColor: cardBgColor
+      }}
+    >
       <Grid container alignItems="center">
         <Grid item xs={4}>
           <Box padding="1em">
-            <img src={mysql} style={{ height: '100%' }} />
+            <SizeMe>
+              {({ size }) => {
+                return (
+                  <Box
+                    style={{
+                      borderRadius: '50%',
+                      border: `3px solid ${iconBorderColor}`,
+                      width: '100%',
+                      height: size.width
+                    }}
+                  >
+                    <img src={iconSrc} style={{ width: '100%' }} />
+                  </Box>
+                );
+              }}
+            </SizeMe>
           </Box>
         </Grid>
         <Divider orientation="vertical" />
         <Grid item xs={8}>
-          <Box padding="2em">s</Box>
+          <Box padding="2em">
+            <Typography variant="h6" style={{ color: '#6e6959' }}>
+              {(label || '').toUpperCase()}
+            </Typography>
+          </Box>
         </Grid>
       </Grid>
     </Card>
--- app/frontend/src/views/Skills/Skills.View.jsx
+++ app/frontend/src/views/Skills/Skills.View.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Box, Grid } from '@material-ui/core';
 import { ComponentLeftAlignedContainer, SkillCard } from 'components';
+import mysql from 'assets/mysql.svg';
+import nodejs from 'assets/nodejs.svg';
+import react from 'assets/react.svg';
+import config from 'config';
+import nodejsbw from 'assets/nodejsbw.svg';
 
 export default function SkillsView({ getter, handler }) {
   return (
@@ -9,15 +14,30 @@ export default function SkillsView({ getter, handler }) {
       <ComponentLeftAlignedContainer maxWidth="xl">
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6} md={3}>
-            <SkillCard />
+            <SkillCard
+              iconSrc={mysql}
+              label="MySQL"
+              cardBgColor={config.styles.colors.tertiary}
+              iconBorderColor={config.styles.colors.quaternary}
+            />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <SkillCard />
+            <SkillCard
+              iconSrc={nodejsbw}
+              label="NodeJS"
+              cardBgColor={config.styles.colors.tertiary}
+              iconBorderColor={config.styles.colors.quaternary}
+            />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <SkillCard />
+            <SkillCard
+              iconSrc={react}
+              label="ReactJS"
+              cardBgColor={config.styles.colors.tertiary}
+              iconBorderColor={config.styles.colors.quaternary}
+            />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
