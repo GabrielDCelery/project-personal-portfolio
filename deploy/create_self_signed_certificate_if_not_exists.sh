47a0994866e8e16714bd1d873f98af16b3de615c
Gabriel Zeller <gabriel.d.celery@gmail.com>
2019-12-13 15:47:50 +0000

    test

--- deploy/create_self_signed_certificate_if_not_exists.sh
+++ deploy/create_self_signed_certificate_if_not_exists.sh
@@ -1,8 +1,9 @@
 FILE_CERT=/etc/letsencrypt/live/gabrielzeller.co.uk/fullchain.pem
 FILE_KEY=/etc/letsencrypt/live/gabrielzeller.co.uk/privkey.pem
 
-if test -f "$FILE_CERT"; then
-  echo "$FILE_CERT exist"
+if [ test -f "$FILE_CERT" ] 
+  then
+    echo "$FILE_CERT exist"
 fi
 
 if test -f "$FILE_KEY"; then
