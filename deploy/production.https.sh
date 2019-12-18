adc0ea121a5afc536a40d99ed5a68144acff773b
Gabriel Zeller <gabriel.d.celery@gmail.com>
2019-12-18 11:43:25 +0000

    - Changed https deploy script

--- deploy/production.https.sh
+++ deploy/production.https.sh
@@ -24,10 +24,9 @@ if [[ ! -f "$FILE_CERT" || ! -f "$FILE_KEY" ]]; then
   up --build --exit-code-from certbot
 fi
 
-INVALID_CERTIFICATE_INDICATOR_TEXT="Fake LE Intermediate X1";
-IS_CERTIFICATE_INVALID=`openssl x509 -in "$FILE_CERT" -text -noout | grep -c "$INVALID_CERTIFICATE_INDICATOR_TEXT"`
+SELF_SIGNED_CERTIFICATE_INDICATOR_TEXT="Fake LE Intermediate X1";
 
-if [[ $IS_CERTIFICATE_INVALID > 0 ]]; then
+if openssl x509 -in "$FILE_CERT" -text -noout | grep -q "$SELF_SIGNED_CERTIFICATE_INDICATOR_TEXT"; then
 	docker-compose \
   -f ./deploy/docker-compose.yml \
   -f ./deploy/docker-compose.prod.https.renewal.yml \
