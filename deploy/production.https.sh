5d0bff815bb37000ae7d0f72aeabd6a791bc20e7
Gabriel Zeller <gabriel.d.celery@gmail.com>
2019-12-25 11:22:06 +0000

    - Changed production deploy script

--- deploy/production.https.sh
+++ deploy/production.https.sh
@@ -1,4 +1,4 @@
-docker stop $(docker ps -a -q)
+docker stop $(docker ps -a -q);
 
 DEPLOY_FOLDER=/var/www/
 PROJECT_FOLDER=project-personal-portfolio
@@ -18,6 +18,7 @@ FILE_CERT=/etc/letsencrypt/live/gabrielzeller.co.uk/fullchain.pem
 FILE_KEY=/etc/letsencrypt/live/gabrielzeller.co.uk/privkey.pem
 
 if [[ ! -f "$FILE_CERT" || ! -f "$FILE_KEY" ]]; then
+  docker stop $(docker ps -a -q);
 	docker-compose \
   -f ./deploy/docker-compose.yml \
   -f ./deploy/docker-compose.prod.https.staging.yml \
@@ -27,12 +28,14 @@ fi
 SELF_SIGNED_CERTIFICATE_INDICATOR_TEXT="Fake LE Intermediate X1";
 
 if openssl x509 -in "$FILE_CERT" -text -noout | grep -q "$SELF_SIGNED_CERTIFICATE_INDICATOR_TEXT"; then
-	docker-compose \
+  docker stop $(docker ps -a -q);
+  docker-compose \
   -f ./deploy/docker-compose.yml \
   -f ./deploy/docker-compose.prod.https.renewal.yml \
   up --build --exit-code-from certbot
 fi
 
+docker stop $(docker ps -a -q);
 docker-compose \
 -f ./deploy/docker-compose.yml \
 -f ./deploy/docker-compose.prod.https.yml \
