171a62c2bf31cbfb7b0de972fb05067f41aa8846
Gabriel Zeller <gabriel.d.celery@gmail.com>
2020-01-01 17:35:33 +0000

    - Added resizable image feature

--- app/frontend/src/components/dataDisplay/AchievmentCard.jsx
+++ app/frontend/src/components/dataDisplay/AchievmentCard.jsx
@@ -13,17 +13,55 @@ import {
 } from '@material-ui/core';
 import styled from 'styled-components';
 
+import { SizeMe } from 'react-sizeme';
 import { GitHub as GitHubIcon } from '@material-ui/icons';
 
 import config from 'config';
 import _ from 'lodash';
 
-import unleashTheGeekPreviewImage from 'assets/images/achievment_unleash_the_geek_001.jpg';
-import halitePreviewImage from 'assets/images/achievment_halite3_001.jpg';
+import achievment_halite_3_320 from 'assets/images/achievment_halite_3_320.jpg';
+import achievment_halite_3_640 from 'assets/images/achievment_halite_3_640.jpg';
+import achievment_halite_3_800 from 'assets/images/achievment_halite_3_800.jpg';
+import achievment_halite_3_1024 from 'assets/images/achievment_halite_3_1024.jpg';
+import achievment_halite_3_1280 from 'assets/images/achievment_halite_3_1280.jpg';
 
-const previewImageSrcMap = {
-  unleashTheGeekPreviewImage,
-  halitePreviewImage
+import achievment_unleash_the_geek_320 from 'assets/images/achievment_unleash_the_geek_320.jpg';
+import achievment_unleash_the_geek_640 from 'assets/images/achievment_unleash_the_geek_640.jpg';
+import achievment_unleash_the_geek_800 from 'assets/images/achievment_unleash_the_geek_800.jpg';
+import achievment_unleash_the_geek_1024 from 'assets/images/achievment_unleash_the_geek_1024.jpg';
+import achievment_unleash_the_geek_1280 from 'assets/images/achievment_unleash_the_geek_1280.jpg';
+
+import placeholder_img from 'assets/images/1x1.png';
+
+const previewImageUrlsSrcMap = {
+  achievment_halite_3_320,
+  achievment_halite_3_640,
+  achievment_halite_3_800,
+  achievment_halite_3_1024,
+  achievment_halite_3_1280,
+  achievment_unleash_the_geek_320,
+  achievment_unleash_the_geek_640,
+  achievment_unleash_the_geek_800,
+  achievment_unleash_the_geek_1024,
+  achievment_unleash_the_geek_1280
+};
+
+const imageSizes = [320, 640, 800, 1024, 1280];
+
+const getImageUrl = ({ imageName, containerSize }) => {
+  if (containerSize === undefined) {
+    return placeholder_img;
+  }
+
+  for (let i = 0, iMax = imageSizes.length; i < iMax; i++) {
+    if (containerSize < imageSizes[i]) {
+      return previewImageUrlsSrcMap[`${imageName}_${imageSizes[i]}`];
+    }
+  }
+
+  return previewImageUrlsSrcMap[
+    `${imageName}_${imageSizes[imageSizes.length - 1]}`
+  ];
 };
 
 const useStyles = makeStyles(theme => ({
@@ -97,11 +135,23 @@ export const AchievmentCard = props => {
         title={title || ''}
         subheader={subTitle || ''}
       />
-      <CardMedia
-        className={classes.media}
-        image={previewImageSrcMap[previewImage]}
-        title="Paella dish"
-      />
+      <SizeMe>
+        {({ size }) => {
+          const { width } = size;
+          const imgUrl = getImageUrl({
+            imageName: previewImage,
+            containerSize: width
+          });
+
+          return (
+            <CardMedia
+              className={classes.media}
+              image={imgUrl}
+              title="Paella dish"
+            />
+          );
+        }}
+      </SizeMe>
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
           {description}
--- app/frontend/src/services/api/competitions.js
+++ app/frontend/src/services/api/competitions.js
@@ -8,7 +8,7 @@ class Competitions {
         codeUrl: 'https://github.com/GabrielDCelery/competition-halite-iii',
         description:
           'Halite III is a resource management game in which players build and command ships that explore the ocean and collect   halite. Ships use halite as an energy source, and the player with the most stored halite at the end of the game is the winner.',
-        previewImage: 'halitePreviewImage',
+        previewImage: 'achievment_halite_3',
         finalPlace: 222,
         numOfCompetitors: 4014
       },
@@ -21,7 +21,7 @@ class Competitions {
           'https://github.com/GabrielDCelery/competition-codingame-unleash-the-geek',
         description:
           'Both players control a team of several robots. The teams start out at the same points on the map, at the headquarters. The robots can use radars from the headquarters to detect and mine   Amadeusium veins. They may also trap certain areas of the map  with EMP traps. These can be triggered by robots which are  then rendered inoperable.',
-        previewImage: 'unleashTheGeekPreviewImage',
+        previewImage: 'achievment_unleash_the_geek',
         finalPlace: 1151,
         numOfCompetitors: 2162
       }
