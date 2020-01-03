8fb021ab1c0f2ced3ce793f2a796ce4a3c0c8a73
Gabriel Zeller <gabriel.d.celery@gmail.com>
2020-01-03 09:02:35 +0000

    - Removed redundant code from WorkHistory

--- app/frontend/src/views/WorkHistory/index.jsx
+++ app/frontend/src/views/WorkHistory/index.jsx
@@ -1,8 +1,7 @@
 import Store from './WorkHistory.Store';
 import Behaviour from './WorkHistory.Behaviour';
-import Styles from './WorkHistory.Styles';
 import View from './WorkHistory.View';
 
-const WorkHistory = Store(Behaviour(Styles(View)));
+const WorkHistory = Store(Behaviour(View));
 
 export { WorkHistory };
